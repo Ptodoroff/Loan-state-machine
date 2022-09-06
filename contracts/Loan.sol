2f1aa92342d02cfb3d7a7c1516d6b1287c125eff
Petar <p.todorov212@gmail.com>
2022-09-06 16:55:21 +0300

    finalised the contract

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -11,6 +11,17 @@ enum State {
 
 
 State public state = State.PENDING;               // a variable that storest the current state of the enum
+//@ notice  under the hood, Solidity sees every variable in an enum as an integer. e.g. PENDING is 0, ACTIVE is 1 and so on. aka enum values are convertible to integers and inegers are converted to enum values
+//@ notice   in this regard, I cannot simply access a variable in an enum by ots corresponding integer, because solidity will complain. Aka I cannot access PENDING by typing State.0. Instead I it happens like this:
+
+/*
+function foo (uint a){
+        state = State(a)       state now equals State.PENDING
+        //in other words, if I want to acess a variable in an enum the syntax is EnunNAME(int);
+}
+
+*/
+
 //@ notice parameters defining the loan
 
 uint public amount;
@@ -31,6 +42,7 @@ constructor (uint _amount, uint _interest, uint _duration, address payable _borr
 function fundLoan () external payable {
         require(amount == msg.value, "You should lend the amount, defined in the constructor");
         require(lender == msg.sender, "Callable only by the lender");  
+        _transitionTo(State.ACTIVE);                        //placed before the actual transfer, because if this function fails to execute, the funds wont be transferred to the borrower
         borrower.transfer(amount);                         //transfers the deposited funds to the borrower                                 
 
 }
@@ -38,9 +50,25 @@ function fundLoan () external payable {
 function repay () external payable {
         require(msg.value==amount + interest, "You should return the interest and the amount");
         require(msg.sender==borrower, "you are not the borrower");
-        require(block.timestamp<=end, "You are late");
+        _transitionTo(State.CLOSED ); 
         borrower.transfer(msg.value); 
 }
 
+function _transitionTo (State to) private {                                                        // declared with _ because this is the ocnvention to followe when declaring priate functions. this fn will change the state of the enum after each of the other functions in the contract. The parameter represents the value of the State enum to which we want the state changed to 
+        require( to != State.PENDING, "Cannot go back to pending state");                          // this check is here to make sure that the the state cannot go back to its initial state 
+        require (to != state, "cannot transition to the initial state" );                          //checks to make sure that state wont change to the current state
+        if ( to == State.ACTIVE) {
+                require (state == State.PENDING, "Cannot transiotion to active state if the current state is NOT PENDING ");
+                state=State.ACTIVE;
+        }
+                if ( to == State.CLOSED) {
+                require (state == State.ACTIVE, "Cannot transiotion to closed state if the current state is NOT ACTIVE ");
+                require(block.timestamp>=end, "Loan has not matured yet. ");
+                state=State.CLOSED;
+        }
+
+
+
+}
 
 }
